5c26778533d7ace41b53635e6e12365ecda60a59
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-05-09 18:04:25 -0300

    header configuration for sheets data table

--- pages/sheets.vue
+++ pages/sheets.vue
@@ -2,12 +2,25 @@
   <v-card>
     <v-card-title>Tabelas</v-card-title>
     <v-divider />
-    <v-data-table />
+    <v-data-table :headers="dataTable.headers" />
   </v-card>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'SheetsPage',
+
+  data: () => ({
+    dataTable: {
+      headers: [
+        { text: 'Disciplina' },
+        { text: 'Ano' },
+        { text: 'Semestre' },
+        { text: 'Excluir', value: 'delete' }
+      ]
+    }
+  })
+};
 </script>
 
 <style></style>
