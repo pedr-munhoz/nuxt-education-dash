d3a7f6872fa769f8d825b85af0f8e28abc5220dd
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-05-09 18:21:52 -0300

    configurations page data table basic setup

--- pages/configuration.vue
+++ pages/configuration.vue
@@ -2,12 +2,37 @@
   <v-card>
     <v-card-title>Configurações</v-card-title>
     <v-divider />
-    <v-data-table />
+    <v-data-table :headers="dataTable.headers" :items="fields" :item-key="id">
+      <template #item.edit>
+        <v-btn outlined small>
+          editar
+        </v-btn>
+      </template>
+    </v-data-table>
   </v-card>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'ConfigurationPage',
+
+  data: () => ({
+    dataTable: {
+      headers: [
+        { text: 'Campo', value: 'title' },
+        { text: 'Literal na tabela', value: 'value' },
+        { text: 'Editar', value: 'edit' }
+      ]
+    },
+
+    fields: [
+      { title: 'ID aluno', id: 1, value: '' },
+      { title: 'Presença', id: 2, value: '' },
+      { title: 'Média', id: 3, value: '' },
+      { title: 'Aprovação', id: 4, value: '' }
+    ]
+  })
+};
 </script>
 
 <style></style>
