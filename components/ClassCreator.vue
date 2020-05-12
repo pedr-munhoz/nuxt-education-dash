5cf16111446e1f423a63b4dee217828ca6b91b8e
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-05-12 10:31:07 -0300

    class creator dialog basic setup

---
+++ components/ClassCreator.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-dialog v-model="dialog" width="640px">
+    <template #octivator="{ on }">
+      <v-btn icon outlined v-on="on">
+        <v-icon>fas fa-plus</v-icon>
+      </v-btn>
+    </template>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'ClassCreator',
+
+  data: () => ({
+    dialog: false
+  })
+};
+</script>
