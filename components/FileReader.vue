b304d8817a99be3b9235ed7e3ff619b1cccfd232
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-05-12 15:11:33 -0300

    file reader working

---
+++ components/FileReader.vue
@@ -0,0 +1,45 @@
+<template>
+  <label class="text-reader">
+    <input type="file" @change="loadTextFromFile" />
+  </label>
+</template>
+
+<script>
+export default {
+  methods: {
+    loadTextFromFile(ev) {
+      const file = ev.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (e) => this.$emit('load', e.target.result);
+      reader.readAsText(file);
+    }
+  }
+};
+</script>
+
+<script>
+import XLSX from 'xlsx';
+export default {
+  name: 'FileReader',
+  methods: {
+    loadTextFromFile(ev) {
+      const file = ev.target.files[0];
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+
+      reader.onload = (e) => {
+        var data = new Uint8Array(reader.result);
+        var wb = XLSX.read(data, { type: 'array' });
+        console.log(wb);
+        var sheet = {};
+        sheet.name = wb.SheetNames[0];
+        sheet.data = wb.Sheets[sheet.name];
+        var json = XLSX.utils.sheet_to_json(sheet.data);
+
+        this.$emit('load', json);
+      };
+    }
+  }
+};
+</script>
