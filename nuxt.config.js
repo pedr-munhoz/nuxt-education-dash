634aa925bc973a1f7d07ccf6e640b3ffd671f09c
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-05-18 17:26:45 -0300

    prettier trailing comma

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,10 +14,10 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+        content: process.env.npm_package_description || '',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
    ** Customize the progress-bar color
@@ -28,7 +28,7 @@ export default {
    */
   css: [
     '@fortawesome/fontawesome-free/css/all.css',
-    '@fortawesome/fontawesome-free/js/all.js'
+    '@fortawesome/fontawesome-free/js/all.js',
   ],
   /*
    ** Plugins to load before mounting the App
@@ -40,14 +40,14 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
   ],
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
   ],
   /*
    ** Axios module configuration
@@ -70,10 +70,10 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
   /*
    ** Build configuration
@@ -82,6 +82,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  }
+    extend(config, ctx) {},
+  },
 };
