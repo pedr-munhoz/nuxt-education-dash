c034f0114bc8a45d782f5e971adb05d8613b1dec
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-05-19 17:17:47 -0300

    home page mounts and displays first chart

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,67 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div>
+    <!--
+    -->
+    <apex-chart
+      v-if="trigger"
+      width="500"
+      type="bar"
+      :options="chartOptions"
+      :series="series"
+    ></apex-chart>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
-import VuetifyLogo from '~/components/VuetifyLogo.vue';
+// Include Dependencies
+// import VueApexCharts from 'vue-apexcharts';
 
 export default {
+  name: 'HomePage',
+
   components: {
-    Logo,
-    VuetifyLogo,
+    // eslint-disable-next-line vue/no-unused-components
+    apexChart: () => import('vue-apexcharts'),
+  },
+
+  data: () => ({
+    trigger: false,
+    chartOptions: {
+      chart: {
+        id: 'vuechart-example',
+      },
+      xaxis: {
+        categories: [],
+      },
+    },
+    series: [],
+    loadTest: [],
+  }),
+
+  computed: {
+    averageVsIteration() {
+      return this.$store.getters['charts/averageVsIteration/getClasses'];
+    },
+  },
+
+  mounted() {
+    this.mountChart();
+    this.trigger = true;
+  },
+
+  methods: {
+    mountChart() {
+      const categories = this.$store.getters[
+        'charts/averageVsIteration/getCategories'
+      ];
+      const classes = this.$store.getters[
+        'charts/averageVsIteration/getClasses'
+      ];
+      this.chartOptions.xaxis.categories = [...categories];
+      classes.forEach((element, index) => {
+        this.series.push({ name: element.title, data: element.data });
+      });
+    },
   },
 };
 </script>
