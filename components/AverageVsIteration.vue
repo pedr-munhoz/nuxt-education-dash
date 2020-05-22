05ef74b0c5e11e63566f16cc0977a12d026ba1b7
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-05-22 19:53:32 -0300

    avg vs it chart now has its own component

---
+++ components/AverageVsIteration.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-card>
+    <apex-chart
+      v-if="trigger"
+      width="90%"
+      height="300px"
+      type="bar"
+      :options="chartOptions"
+      :series="series"
+    />
+  </v-card>
+</template>
+
+<script>
+// Include Dependencies (ERROR, lazy load fix)
+// import VueApexCharts from 'vue-apexcharts';
+
+export default {
+  name: 'AverageVsIteration',
+
+  components: {
+    // lazy load necessary, but component is only started on mounted
+    apexChart: () => import('vue-apexcharts'),
+  },
+
+  data: () => ({
+    // trigger used to activate the charts after the lazy load
+    trigger: false,
+    chartOptions: {
+      chart: {
+        id: 'vuechart-example',
+      },
+      xaxis: {
+        categories: [],
+      },
+    },
+    series: [],
+  }),
+
+  computed: {},
+
+  mounted() {
+    this.mountChart();
+    this.trigger = true;
+  },
+
+  methods: {
+    mountChart() {
+      // returs the year+semester combinations with avaliable data
+      const categories = this.$store.getters[
+        'charts/averageVsIteration/getIterations'
+      ];
+      // sets the year+semester combinations on the x axis
+      this.chartOptions = {
+        ...this.chartOptions,
+        ...{
+          xaxis: {
+            categories: [],
+          },
+        },
+      };
+      this.chartOptions.xaxis.categories = [...categories];
+
+      // returns the averages by year/semester on all avaliable classes
+      const classes = this.$store.getters[
+        'charts/averageVsIteration/getClasses'
+      ];
+      // sets each class with a series on the chart
+      this.series = [];
+      classes.forEach((element, index) => {
+        this.series.push({ name: element.title, data: element.data });
+      });
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,25 @@
 <template>
   <div>
-    <apex-chart
-      v-if="trigger"
-      width="80%"
-      height="300px"
-      type="bar"
-      :options="chartOptions"
-      :series="series"
-    />
+    <average-vs-iteration />
   </div>
 </template>
 
 <script>
-// Include Dependencies (ERROR, lazy load fix)
-// import VueApexCharts from 'vue-apexcharts';
+import AverageVsIteration from '@/components/AverageVsIteration.vue';
 
 export default {
   name: 'HomePage',
 
   components: {
-    // lazy load necessary, but component is only started on mounted
-    apexChart: () => import('vue-apexcharts'),
+    AverageVsIteration,
   },
 
-  data: () => ({
-    // trigger used to activate the charts after the lazy load
-    trigger: false,
-    chartOptions: {
-      chart: {
-        id: 'vuechart-example',
-      },
-      xaxis: {
-        categories: [],
-      },
-    },
-    series: [],
-  }),
+  data: () => ({}),
 
   computed: {},
 
-  mounted() {
-    this.mountChart();
-    this.trigger = true;
-  },
+  mounted() {},
 
-  methods: {
-    mountChart() {
-      // returs the year+semester combinations with avaliable data
-      const categories = this.$store.getters[
-        'charts/averageVsIteration/getIterations'
-      ];
-      // sets the year+semester combinations on the x axis
-      this.chartOptions = {
-        ...this.chartOptions,
-        ...{
-          xaxis: {
-            categories: [],
-          },
-        },
-      };
-      this.chartOptions.xaxis.categories = [...categories];
-
-      // returns the averages by year/semester on all avaliable classes
-      const classes = this.$store.getters[
-        'charts/averageVsIteration/getClasses'
-      ];
-      // sets each class with a series on the chart
-      this.series = [];
-      classes.forEach((element, index) => {
-        this.series.push({ name: element.title, data: element.data });
-      });
-    },
-  },
+  methods: {},
 };
 </script>
