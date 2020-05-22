5699d8901649d174f28421fb923d2883204a1aa7
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-05-22 20:44:54 -0300

    charts module reworked: stored and parsing done in index file

--- store/charts/averageVsIteration.js
+++ store/charts/averageVsIteration.js
@@ -1,21 +1,16 @@
-export const state = () => ({
-  // classes [id,[{ year: number, semester: number(1/2), average: number' id: string}]]
-  classes: [],
-});
-
 export const getters = {
-  getClasses(state, getters) {
+  getClasses(state, getters, rootState) {
     const returnClasses = [];
     // criate an object for every year/semester in record
     const iterations = getters.getIterations;
-    state.classes.forEach((element) => {
+    rootState.charts.classes.forEach((element) => {
       returnClasses.push({ id: element.id, title: element.title });
     });
     returnClasses.forEach((returnClass) => {
       returnClass.data = [];
       iterations.forEach((category) => {
         // finds the class matching the current class.id
-        const classObj = state.classes.find(
+        const classObj = rootState.charts.classes.find(
           (element) => element.id === returnClass.id,
         );
         // finds the register of the class in this year/semester
@@ -30,21 +25,13 @@ export const getters = {
     });
     return returnClasses;
   },
-  getClassesPure(state) {
-    return state.classes;
-  },
-  getClassIndex: (state, getters) => (possibleClass) => {
-    const theClass = getters.getClassesPure.find(
-      (element) => element.id === possibleClass.id,
-    );
-    return getters.getClassesPure.indexOf(theClass);
-  },
-  getIterations(state) {
-    if (state.classes.length === 0) {
+
+  getIterations(state, getters, rootState) {
+    if (rootState.charts.classes.length === 0) {
       return [];
     }
     const sheets = [];
-    state.classes.forEach((element) => {
+    rootState.charts.classes.forEach((element) => {
       element.data.forEach((e) => {
         sheets.push({ year: e.year, semester: e.semester });
       });
@@ -63,64 +50,5 @@ export const getters = {
     }
     const uniques = [...new Set(sheets.iterations)];
     return uniques;
-  },
-};
-
-export const mutations = {
-  ADD_CLASS(state, newClass) {
-    state.classes.push(newClass);
-  },
-  SET_CLASS(state, classIteration) {
-    const theClass = state.classes.find(
-      (element) => element.id === classIteration.id,
-    );
-    if (theClass) {
-      classIteration.data.id = `${classIteration.data.year}.${classIteration.data.semester}`;
-      theClass.data.push(classIteration.data);
-    }
-  },
-  REMOVE_CLASS(state, index) {
-    state.classes.splice(index, 1);
-  },
-};
-
-export const actions = {
-  parseSheet({ dispatch }, sheet) {
-    const filteredAverages = sheet.data.filter(
-      (element) => element.final_score,
-    );
-    const sum = filteredAverages.reduce((sum, element) => {
-      return sum + element.final_score;
-    }, 0);
-    const average = sum / filteredAverages.length;
-    const classIteration = {
-      id: sheet.class.id,
-      title: sheet.class.title,
-      data: {
-        year: sheet.year,
-        semester: sheet.semester,
-        average: average.toFixed(1),
-      },
-    };
-    dispatch('includeClassIteration', classIteration);
-  },
-  // recieves a class object (one year+semester instance) and adds it to the state
-  includeClassIteration({ commit, getters }, classIteration) {
-    if (getters.getClassIndex(classIteration) >= 0) {
-      commit('SET_CLASS', classIteration);
-    } else {
-      classIteration.data.id = `${classIteration.data.year}.${classIteration.data.semester}`;
-      commit('ADD_CLASS', {
-        id: classIteration.id,
-        title: classIteration.title,
-        data: [classIteration.data],
-      });
-    }
-  },
-  // recieves a class object and deletes all data of it based on the .id attribute
-  deleteClass({ commit, getters }, oldClass) {
-    if (getters.getClassIndex(oldClass) >= 0) {
-      commit('REMOVE_CLASS', getters.getClassIndex(oldClass));
-    }
   },
 };
--- store/charts/index.js
+++ store/charts/index.js
@@ -1,7 +1,79 @@
-export const state = () => ({});
+export const state = () => ({
+  classes: [
+    {
+      id: '230940239',
+      data: [{ id: '2010.2', year: 2010, semester: 2, average: 8, size: 28 }],
+    },
+  ],
+});
+
+export const getters = {
+  getClassesPure(state) {
+    return state.classes;
+  },
+  getClassIndex: (state, getters) => (possibleClass) => {
+    const theClass = getters.getClassesPure.find(
+      (element) => element.id === possibleClass.id,
+    );
+    return getters.getClassesPure.indexOf(theClass);
+  },
+};
+
+export const mutations = {
+  ADD_CLASS(state, newClass) {
+    state.classes.push(newClass);
+  },
+  SET_CLASS(state, classIteration) {
+    const theClass = state.classes.find(
+      (element) => element.id === classIteration.id,
+    );
+    if (theClass) {
+      classIteration.data.id = `${classIteration.data.year}.${classIteration.data.semester}`;
+      theClass.data.push(classIteration.data);
+    }
+  },
+  REMOVE_CLASS(state, index) {
+    state.classes.splice(index, 1);
+  },
+};
 
 export const actions = {
   parseSheet({ dispatch }, sheet) {
-    dispatch('averageVsIteration/parseSheet', sheet);
+    const filteredAverages = sheet.data.filter(
+      (element) => element.final_score,
+    );
+    const sum = filteredAverages.reduce((sum, element) => {
+      return sum + element.final_score;
+    }, 0);
+    const average = sum / filteredAverages.length;
+    const classIteration = {
+      id: sheet.class.id,
+      title: sheet.class.title,
+      data: {
+        year: sheet.year,
+        semester: sheet.semester,
+        average: average.toFixed(1),
+      },
+    };
+    dispatch('includeClassIteration', classIteration);
+  },
+  // recieves a class object (one year+semester instance) and adds it to the state
+  includeClassIteration({ commit, getters }, classIteration) {
+    if (getters.getClassIndex(classIteration) >= 0) {
+      commit('SET_CLASS', classIteration);
+    } else {
+      classIteration.data.id = `${classIteration.data.year}.${classIteration.data.semester}`;
+      commit('ADD_CLASS', {
+        id: classIteration.id,
+        title: classIteration.title,
+        data: [classIteration.data],
+      });
+    }
+  },
+  // recieves a class object and deletes all data of it based on the .id attribute
+  deleteClass({ commit, getters }, oldClass) {
+    if (getters.getClassIndex(oldClass) >= 0) {
+      commit('REMOVE_CLASS', getters.getClassIndex(oldClass));
+    }
   },
 };
