07fcd81ea63f35b7668c030f265cf05a24636b86
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-05-22 20:03:06 -0300

    avg vs class size component created

---
+++ components/AverageVsClassSize.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-card>
+    <apex-chart
+      v-if="trigger"
+      width="90%"
+      height="300px"
+      type="bar"
+      :options="chartOptions"
+      :series="series"
+    />
+  </v-card>
+</template>
+
+<script>
+// Include Dependencies (ERROR, lazy load fix)
+// import VueApexCharts from 'vue-apexcharts';
+
+export default {
+  name: 'AverageVsClasssize',
+
+  components: {
+    // lazy load necessary, but component is only started on mounted
+    apexChart: () => import('vue-apexcharts'),
+  },
+
+  data: () => ({
+    // trigger used to activate the charts after the lazy load
+    trigger: false,
+    chartOptions: {
+      chart: {
+        id: 'vuechart-example',
+      },
+      xaxis: {
+        categories: [],
+      },
+    },
+    series: [],
+  }),
+
+  computed: {},
+
+  mounted() {
+    this.mountChart();
+    this.trigger = true;
+  },
+
+  methods: {
+    mountChart() {
+      this.chartOptions = {
+        ...this.chartOptions,
+        ...{
+          xaxis: {
+            categories: [],
+          },
+        },
+      };
+      this.series = [];
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
+    <average-vs-class-size />
+    <v-divider />
     <average-vs-iteration />
   </div>
 </template>
 
 <script>
 import AverageVsIteration from '@/components/AverageVsIteration.vue';
+import AverageVsClassSize from '@/components/AverageVsClassSize.vue';
 
 export default {
   name: 'HomePage',
 
   components: {
     AverageVsIteration,
+    AverageVsClassSize,
   },
 
   data: () => ({}),
